6688adbaf23e73386faf762c854072eae83cd4a3
deluwarhoisn <babuhossen301@gmail.com>
2025-08-15 22:18:06 +0600

    mileston-05-object

---
+++ object.js
@@ -0,0 +1,27 @@
+const nayok = {
+    name: 'sakhib khan',
+    id: 121,
+    addresh:'movie cinema',
+    isSingele:true,
+    frends:['apu','dipu','antor','shimul'],
+    movish:[{name:'no-1',year:'2025'},{name:'king khan',year:2025}],
+    act: function(){
+        console.log('acting like sakib khane')
+    },
+
+    car:{
+        brand:'tesla',
+        price:5000000,
+        made:2025,
+        manufacture:
+        {
+            name:'tesla',
+            ceo:'elon masuk',
+            made:'usa'
+        }
+    }
+
+}
+// console.log(student.frends)
+console.log(nayok.act)
+nayok.act()
