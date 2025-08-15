5288de87dc075cf988cfc7168383d47db77c5210
deluwarhoisn <babuhossen301@gmail.com>
2025-08-15 22:29:45 +0600

    array

---
+++ array.js
@@ -0,0 +1,12 @@
+// const product = [
+//     {name:'phone',price:12000}
+//     {name:'leptop',price:32000}
+// ]
+// product[0];
+// product[1];
+
+const product = {
+    '0':15,
+    '1':56,
+    '2':87,
+}
