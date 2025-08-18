83051cbf023e2131265fce09d80239f1312d947b
deluwarhoisn <babuhossen301@gmail.com>
2025-08-18 23:54:29 +0600

    h1 tag for dom

--- TOUR_OF_DOM/js/append.js
+++ TOUR_OF_DOM/js/append.js
@@ -6,7 +6,9 @@ const mainContainer = document.getElementById('main-container');
  const PlaceSection = document.createElement('section');
 
 
-
+// crate h1
+const h1 = document.createElement('h1');
+h1.innerText = 'places i want to visit'
 
  // 3.append Placesection to the maincontainer
 
