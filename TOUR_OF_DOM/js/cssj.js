a910e5acd8e649e9736fb911dc4f8513cd65c03f
deluwarhoisn <babuhossen301@gmail.com>
2025-08-18 23:19:38 +0600

    for and dom

---
+++ TOUR_OF_DOM/js/cssj.js
@@ -0,0 +1,12 @@
+
+
+// for(const section of sections){
+// section.classList.add('section-card')
+// }
+
+for(const section of sections){
+    console.log(section);
+    section.style.backgroundColor = 'lightblue'
+    section.style.border = '3px  solid red';
+    section.style.marginBottom = '5px';
+}
